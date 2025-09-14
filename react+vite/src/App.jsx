37c379d83a05e44e8df745bf58617f7d9df84ff0
MSI1 <aryansharma1126@gmail.com>
2025-09-14 17:14:20 +0530

    Changed app.jsx from master

--- react+vite/src/App.jsx
+++ react+vite/src/App.jsx
@@ -7,8 +7,13 @@ export default function App() {
   
   return (
     <div className={styles.card}>
-    <Card title="hello"  surname="sisodia"/>
+    <Card title="hello world"  surname="sisodia"/>
     <Card title="Ansh" surname="sisodia"/>
+    <Card title="Aryan" surname="Sharma"/>
+
+      <Card title="hello world"  surname="sisodia"/>
+    <Card title="Ansh" surname="sisodia"/>
+    <Card title="Aryan" surname="Sharma"/>
 
     </div>
   )
